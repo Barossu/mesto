a076c05d1fef3491ca76589df8966ef0b8d8506b
Pavel Kalashnikov <pashka333d@gmail.com>
2023-07-01 04:03:59 +0300

    fix: Исправлены ошибки после первого ревью

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let editbutton = page.querySelector('.profile__edit-button');
 let popup = page.querySelector('.popup');
 let closebutton = popup.querySelector('.popup__close-icon');
 let formelement = popup.querySelector('.popup__form');
-let nameinput = popup.querySelector('.popup__name-field');
-let jobinput = popup.querySelector('.popup__description-field');
+let nameinput = popup.querySelector('.popup__input_type_name');
+let jobinput = popup.querySelector('.popup__input_type_description');
 let savebutton = popup.querySelector('.popup__button');
 let profilename = page.querySelector('.profile__name');
 let profiledescription = page.querySelector('.profile__description');
@@ -13,11 +13,14 @@ function handleFormSubmit (evt) {
   evt.preventDefault();
   profilename.textContent = nameinput.value;
   profiledescription.textContent = jobinput.value;
+  popup.classList.remove('popup_opened');
 }
 
 function toggleform() {
   if (popup.classList.contains('popup_opened') !== true) {
     popup.classList.add('popup_opened');
+    nameinput.value = profilename.textContent;
+    jobinput.value = profiledescription.textContent;
   }
   else {
     popup.classList.remove('popup_opened');
@@ -26,5 +29,4 @@ function toggleform() {
 
 editbutton.addEventListener('click', toggleform);
 closebutton.addEventListener('click', toggleform);
-savebutton.addEventListener('click', toggleform)
 formelement.addEventListener('submit', handleFormSubmit);
